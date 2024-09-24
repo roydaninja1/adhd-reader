bed8cf46ff3fd4d8e3fac4c22806b31f6f06f8ae
Roy <royyuejie@gmail.com>
2024-09-24 21:50:13 +1000

    Stopped the install page from opening on updates

---
+++ script.js
@@ -0,0 +1,78 @@
+function get_text() {
+    var tree_walker = document.createTreeWalker(
+        document.body,
+        NodeFilter.SHOW_TEXT,
+        function(node) {
+            if (/^\s*$/.test(node.nodeValue)) {
+                return NodeFilter.FILTER_REJECT;
+            }
+            else if (node.parentElement.tagName === "SCRIPT" || node.parentElement.tagName === "STYLE") {
+                return NodeFilter.FILTER_REJECT;
+            }
+            return NodeFilter.FILTER_ACCEPT;
+        }
+    )
+
+    var node;
+    var text_nodes = [];
+    while (node = tree_walker.nextNode()) {
+        text_nodes.push(node);
+    }
+
+    return text_nodes;
+}
+
+function append_div(first_half, second_half, element) {
+    let span = document.createElement("span");
+    span.style.fontWeight = "bold";
+    span.textContent = first_half;
+    let norm = document.createTextNode(second_half);
+    element.appendChild(span);
+    element.appendChild(norm);
+}
+
+function bolden() {
+    var text = get_text();
+    for (var i = 0; i < text.length; i++) {
+        // console.log({text: text[i].nodeValue, parent : text[i].parentElement});
+        var div = document.createElement("div");
+        var word_array = text[i].nodeValue.split(" ");
+        for (var w = 0; w < word_array.length; w++) {
+            var chars = Array.from(word_array[w]);
+            if (chars.length == 1) {
+                append_div(chars, ' ', div);
+                continue;
+            }
+            if (chars.length % 2 == 0) {
+                var middle = (chars.length) / 2;
+                let first_half = chars.slice(0, middle).join('');
+                let second_half = chars.slice(-middle).join('');
+                second_half += " ";
+                append_div(first_half, second_half, div);
+            }
+            else {
+                var middle2 = (chars.length + 1) / 2;
+                let first_half2 = chars.slice(0, middle2).join('');
+                let second_half2 = chars.slice(-(middle2 - 1)).join('');
+                second_half2 += " ";
+                append_div(first_half2, second_half2, div);
+            }
+        }
+        div.style.all = "unset";
+        div.style.display = text[i].parentElement.style.display;
+        text[i].parentNode.replaceChild(div, text[i]);
+    }
+}
+
+// get_text();
+// console.log(document.body.childNodes);
+window.onload = function(){
+    chrome.storage.local.get(["on"]).then(function(result) {
+        if (result["on"] == true) {
+            bolden();
+        }
+        else {
+            ;
+        }
+    });
+};
---
+++ service-worker.js
@@ -0,0 +1,22 @@
+chrome.runtime.onInstalled.addListener(function (object) {
+    if (object.OnInstalledReason === chrome.runtime.OnInstalledReason.install) {
+        chrome.tabs.create({"url" : chrome.runtime.getURL("install.html")}, )
+    }
+});
+
+chrome.action.onClicked.addListener(function() {
+    chrome.action.setBadgeBackgroundColor({"color" : "white"});
+    chrome.action.setBadgeTextColor({"color" : "black"})
+    chrome.storage.local.get(["on"]).then(function(result) {
+        if (result["on"] == true) {
+            chrome.storage.local.set({"on" : false});
+            chrome.action.setBadgeText({"text" : "Off"});
+            chrome.tabs.reload();
+        }
+        else {
+            chrome.storage.local.set({"on" : true});
+            chrome.action.setBadgeText({"text" : "On"});
+            chrome.tabs.reload();
+        }
+    })
+});
